9a8106f5aa21157a9b94cd08be755dc10deb0c6e
diskhkme <diskhkme@gmail.com>
2020-08-30 22:59:34 +0900

    Edit premake for static hazel

--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,33 @@
 project "ImGui"
-  kind "StaticLib"
-  language "C++"
-  
-  targetdir("bin/" .. outputdir .. "/%{prj.name}")
-  objdir("bin-int/" .. outputdir .. "/%{prj.name}")
-  
-  files
-  {
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
-  }
-  
-  filter "system:windows"
-    systemversion "latest"
+    kind "StaticLib"
+    language "C++"
     cppdialect "C++17"
-    staticruntime "On"
+    staticruntime "on"
 
-	  defines
-	  {
-		"IMGUI_API = __declspec(dllexport)"
-	  }
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
   
-  filter {"configurations:Release"}
-	buildoptions "/MT"
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+  
+    filter "system:windows"
+        systemversion "latest"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
